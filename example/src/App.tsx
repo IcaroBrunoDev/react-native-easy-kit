1a3a7d8492a93c8d759711c7f4dcdb95a15d23bd
Ícaro Bruno <icarobrunodev@gmail.com>
2024-01-11 14:06:38 -0300

    chore: package settings

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,46 +2,8 @@ import * as React from 'react';
 
 import { StyleSheet, View } from 'react-native';
 
-import { Accordion, Text } from 'rn-ui-base-components';
-
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Button
-        title="Teste"
-        onPress={() => {
-          console.log('Pressed');
-        }}
-        rounded
-      /> */}
-
-      {/* <NavBar
-        text="Página teste"
-        onIconPress={() => {
-          console.log('Pressed');
-        }}
-        NavIcon={<Icon name="chevron-back" color="#000" size={20} />}
-      /> */}
-
-      {/* <Fab
-        onPress={() => {
-          console.log('FAB Pressed');
-        }}
-        Icon={<Icon name="chevron-back" color="#000" size={20} />}
-      /> */}
-
-      <Accordion title="Teste">
-        <Text style={{ color: '#FFF' }}>
-          This error is located at: in Accordion (created by App) in RCTView
-          (created by View) in View (created by App) in App (created by
-          withDevTools(App)) in withDevTools(App) in RCTView (created by View)
-          in View (created by AppContainer) in RCTView (created by View) in View
-          (created by AppContainer) in AppContainer in main(RootComponent), js
-          engine: hermes
-        </Text>
-      </Accordion>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
